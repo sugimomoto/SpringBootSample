c693985452018b9d81a1ac3de7152876cf71a812
sugimomoto <sugimomoto@gmail.com>
2022-03-08 07:47:41 +0900

    Added chapter 3.3

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
+        return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false) String message, Model model){
+        model.addAttribute("sample", message);
+        return "hello";
+    }
+
+
+    @GetMapping("/greeting2")
+    public String greeting2(@RequestParam(defaultValue = "Hello!") String message, Model model){
+        model.addAttribute("sample", message);
         return "hello";
     }
 }
