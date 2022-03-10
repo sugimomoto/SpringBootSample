afd66dd5f0488eb088aecfb6bb647b652a8cf5c1
sugimomoto <sugimomoto@gmail.com>
2022-03-11 07:50:39 +0900

    Fixed lombok settings

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -7,22 +7,4 @@ public class User {
     private String name;
     private String email;
     private Integer age;
-    public String getName() {
-        return name;
-    }
-    public Integer getAge() {
-        return age;
-    }
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
